edb131db1e572f7176790b28d74fd23fc668ff29
Seth Junot <xsetech@gmail.com>
2021-12-02 00:20:29 -0800

    Implemented solution for day 1 part 2
    "1683" was accepted

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::process;
 
 
-fn part1(input: Vec<i32>) -> i32 {
+fn part1(input: &Vec<i32>) -> i32 {
 
     let mut r: i32 = 0;
     let mut x: i32 = input[0];
@@ -19,6 +19,30 @@ fn part1(input: Vec<i32>) -> i32 {
 
 }
 
+fn slide_sum_over(input: &Vec<i32>) -> Vec<i32> {
+
+    if input.len() < 3 {
+        eprintln!("minimum of three inputs are required for part 2");
+        process::exit(1);
+    }
+
+    let num_sums: usize = input.len() - 2;
+    let mut output: Vec<i32> = Vec::with_capacity(num_sums);
+    for i in 0..num_sums {
+        let sum = input[i] + input[i + 1] + input[i + 2];
+        output.push(sum);
+    }
+
+    return output;
+
+}
+
+fn part2(input: &Vec<i32>) -> i32 {
+
+    let intermediate = slide_sum_over(input);
+    return part1(&intermediate);
+
+}
 
 fn main() {
 
@@ -36,9 +60,13 @@ fn main() {
         |l| l.parse().expect("error in input file")
     ).collect();
     if part1_input.len() > 0 {  // let empty file mean "skip"
-        let part1_answer = part1(part1_input);
+        let part1_answer = part1(&part1_input);
         println!("Part 1 answer: {}", part1_answer);
     }
+
+    println!("Part 2 has no input.");
+    let part2_answer = part2(&part1_input);
+    println!("Part 2 answer: {}", part2_answer);
 
 }
 
@@ -61,8 +89,42 @@ mod tests {
             260, // (decreased)
             263, // (increased)
         ];
-        let output = part1(example_inputs.to_vec());
+        let output = part1(&example_inputs.to_vec());
         assert_eq!(7, output);
+    }
+
+    #[test]
+    fn test_part2_example() {
+
+        let example_inputs_1 = [
+            199,  // A
+            200,  // A B
+            208,  // A B C
+            210,  //   B C D
+            200,  // E   C D
+            207,  // E F   D
+            240,  // E F G
+            269,  //   F G H
+            260,  //     G H
+            263,  //       H
+        ];
+
+        let example_inputs_2 = [
+            607,  // A (N/A - no previous sum)
+            618,  // B (increased)
+            618,  // C (no change)
+            617,  // D (decreased)
+            647,  // E (increased)
+            716,  // F (increased)
+            769,  // G (increased)
+            792,  // H (increased)
+        ];
+
+        let intermediate = slide_sum_over(&example_inputs_1.to_vec());
+        assert_eq!(intermediate, example_inputs_2);
+
+        let output = part2(&example_inputs_1.to_vec());
+        assert_eq!(5, output);
     }
 
 }
