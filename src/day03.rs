261a711121c91a14fac372caab91edbd5d775b9d
Seth Junot <xsetech@gmail.com>
2021-12-07 03:45:40 -0800

    Refactor day 3 part 1
    Refactoring after reading the requirements for part 2, which require
    calling the most-common-bit function but with different behavior for
    ties.

--- src/day03.rs
+++ src/day03.rs
@@ -3,13 +3,72 @@
 use std::fs;
 use std::path::Path;
 
+pub struct Input {
+    pub width: usize,
+    pub data: Vec<u32>,
+}
+
+
+/// Return a bit mask representing the most common bit per column from rows of
+/// equally size (give by Input.width) integers.
+///
+/// Example:
+///
+/// ```
+/// let i = Input {
+///     width: 3 as usize,
+///     data: vec![
+///         0b010
+///         0b111
+///         0b110
+///     ],
+/// }
+/// assert_eq!(0b110, most_common_bit(&i, None));
+/// ```
+///
+/// A "tie breaker" can be given if there's a tie between 0 or 1 for a column.
+/// The value is either 0 or 1, whichever will win the tie by default. If there
+/// is a tie without a tie breaker, the code will panic.
+pub fn most_common_bit(input: &Input, tie_breaker: Option<u32>) -> u32 {
+
+    let mut counters = vec![0; input.width];
+
+    for int in input.data.iter() {
+        for offset in 0..input.width {
+            let mask = 1 << offset as u32;
+            if (mask & int) == mask {
+                counters[offset] += 1;
+            } else {
+                counters[offset] -= 1;
+            }
+        }
+    }
+
+    let value_parts = counters.iter().rev().map(
+        |val| {
+            if *val > 0 { 1 }
+            else if *val < 0 { 0 }
+            else {
+                // tie!
+                let tb = tie_breaker.expect("tie without a tie breaker");
+                if tb == 1 { 1 } else { 0 }
+            }
+        }
+    );
+
+    let mut gamma_rate: u32 = 0;
+    for part in value_parts {
+        gamma_rate <<= 1;
+        gamma_rate += part;
+    }
+    return gamma_rate;
+
+}
+
 
 mod part1 {
 
-    pub struct Input {
-        width: usize,
-        data: Vec<u32>,
-    }
+    use super::*;
 
     pub fn input(source: &str) -> Input {
         let lines: Vec<&str> = source.lines().collect();
@@ -24,32 +83,7 @@ mod part1 {
 
     pub fn solution(input: &Input) -> u32 {
 
-        let mut counters = vec![0; input.width];
-
-        for int in input.data.iter() {
-            for offset in 0..input.width {
-                let mask = 1 << offset as u32;
-                if (mask & int) == mask {
-                    counters[offset] += 1;
-                } else {
-                    counters[offset] -= 1;
-                }
-            }
-        }
-
-        let value_parts = counters.iter().rev().map(
-            |val| {
-                if *val == 0 { panic!("ambiguity") };
-                if *val > 0 { 1 } else { 0 }
-            }
-        );
-
-        let mut gamma_rate: u32 = 0;
-        for part in value_parts {
-            gamma_rate <<= 1;
-            gamma_rate += part;
-        }
-
+        let gamma_rate = most_common_bit(input, None);
         let mask = (1 << input.width) - 1;
         let epsilon_rate: u32 = !gamma_rate & mask;
 
