de84f10bf0ad7a25cbe8c4354291c0a81547513f
Seth Junot <xsetech@gmail.com>
2021-12-11 11:20:45 -0800

    Fix utils::remove_multiline_whitespace() should only empty lines at str start and end

--- src/utils.rs
+++ src/utils.rs
@@ -19,13 +19,18 @@
 pub fn remove_multiline_whitespace(source: &str) -> String {
 
     let lines: Vec<&str> = source.trim().lines().collect();
+    let num_lines = lines.len();
     let mut lines_stripped: Vec<&str> = Vec::new();
 
-    for line in lines.iter() {
+    for line_idx in 0..num_lines {
+        let line = lines[line_idx];
         let line_stripped = line.trim();
-        if !line_stripped.is_empty() {
-            lines_stripped.push(line_stripped);
+        if line_idx == 0 || line_idx == num_lines - 1 {
+            if line_stripped.is_empty() {
+                continue;
+            }
         }
+        lines_stripped.push(line_stripped);
     }
 
     return lines_stripped.join("\n");
@@ -45,6 +50,25 @@ mod tests {
             b
         ";
         let y = "a\nb";
+        let z = remove_multiline_whitespace(&x);
+
+        assert_ne!(x, y);  // sanity check
+        assert_eq!(y, z);
+
+    }
+
+    #[test]
+    fn test_middle_newlines_preserved() {
+
+        let x = "
+            a
+
+            b
+            c
+
+            d
+        ";
+        let y = "a\n\nb\nc\n\nd";
         let z = remove_multiline_whitespace(&x);
 
         assert_ne!(x, y);  // sanity check
